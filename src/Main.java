fa005cdd26a4915f9c3792a812406cff3258aa52
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-10-12 21:52:20 +0300

    lesson 4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
+        //Scanner scanner = new Scanner(System.in);
+        //int num = scanner.nextInt();
        /*if (num >=9 && num <=11){
             System.out.println("fall");
         } else if (num>=3 && num <=5) {
@@ -31,11 +32,56 @@ public class Main {
             /*for (int i = 2; i <= 20; i += 2) {
                 if(i>10) {System.out.println(i);}
             }*/
-        for (int i = 1; i <= num + 200; i++) {
+       /* for (int i = 1; i <= num + 200; i++) {
             if (num % i == 0) {
                 System.out.println(i);
             }
         }
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");*/
+
+        /*int[] r = new int[5];
+        for (int i = 0; i < 5; i++) {
+            r[i] = (int) (Math.random() * 100);
+            System.out.println(r[i] + "^2 = " + (int) (Math.pow(r[i], 2)));
+        }*/
+        Scanner scanner = new Scanner(System.in);
+        /*String[] array = new String[3];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextLine();
+        }
+        System.out.println("Array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }*/
+        //----------------------
+        /** Array */
+       /* String[] fruits = new String[4];
+        for (int i = 0; i < fruits.length; i++) {
+            fruits[i] = scanner.nextLine();
+        }
+        System.out.println(fruits[1] + " " + fruits[3]);
+        System.out.println(fruits.length);
+        fruits[2] = "banana";
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }*/
+
+        int size = scanner.nextInt();
+        double[] masDouble = new double[size];
+        for (int i = 0; i < size; i++) {
+            masDouble[i] = Math.random() * 100;
+            System.out.println(masDouble[i]);
+        }
+        for (int i = 1; i < size; i += 2) {
+            masDouble[i] = masDouble[i] * masDouble[i];
+        }
+        System.out.println("-----------------------");
+        for (int i = 0; i < size; i++) {
+            System.out.println(masDouble[i]);
+        }
+        System.out.println("----------------------");
+        for (int i = size - 1; i >= 0; i--) {
+            System.out.println(masDouble[i]);
+        }
     }
 }
